b0124472b072f4f1b58b7f0a0fa1f3f3f77757b1
emreceran <emrecrn@gmail.com>
2026-01-19 18:49:24 +0300

    wizard iptal

--- __manifest__.py
+++ __manifest__.py
@@ -8,7 +8,7 @@
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
-        'wizard/vault_fetch_wizard_view.xml',
+        # 'wizard/vault_fetch_wizard_view.xml',
         'views/views.xml',
     ],
     'installable': True,

fe3c773b6b5d93dc4d7ff6153c8b4cd61e402de1
emreceran <emrecrn@gmail.com>
2026-01-23 16:06:51 +0300

    bölge

--- models/models.py
+++ models/models.py
@@ -74,8 +74,10 @@ class ResUsers(models.Model):
         string='Rehberimdeki Kişiler'
     )
 
-    # region = fields.Selection([
-    #     ('istanbul', 'İstanbul'),
-    #     ('ankara', 'Ankara'),
-    #     ('konya', 'Konya')
-    # ], string="Bölge", default=False,  help="Kullanıcının bağlı olduğu bölgeyi seçiniz.")
+    region = fields.Selection([
+        ('ik', 'İstanbul Koalisyon'),
+        ('if', 'İstanbul Danışman'),
+        ('bursa', 'Bursa'),
+        ('ankara', 'Ankara'),
+        ('konya', 'Konya')
+    ], string="Bölge", default=False,  help="Kullanıcının bağlı olduğu bölgeyi seçiniz.")

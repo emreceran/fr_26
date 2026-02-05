8a40bd7d8939d480f2b1015a63fec3add36a8704
emreceran <emrecrn@gmail.com>
2026-02-05 12:58:45 +0300

    sicilno7

--- models/models.py
+++ models/models.py
@@ -73,7 +73,7 @@ class ResPartner(models.Model):
         # default değeri vermiyoruz, böylece boş gelebilir.
     )
     ozel_il_id = fields.Char(string='Şehir (İl)', help="Plaka kodlu özel il seçimi")
-    secime_girdi = fields.Boolean(string='Seçime Girdi', default=False)
+    secime_girdi = fields.Boolean(string='Seçime Girdi', default=False, store=False)
 
 
 
